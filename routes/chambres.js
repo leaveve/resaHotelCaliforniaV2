e97e62c5e31d32dd39c646240cfd2fd858f7b0fc
Batuhan-SIO <batuhanerbek2@gmail.com>
2025-12-11 14:01:22 +0100

    ooooop

--- routes/chambres.js
+++ routes/chambres.js
@@ -4,14 +4,14 @@ import ChambreController from '../controllers/chambreController.js';
 const router = express.Router();
 // Validation des données de chambre
 const chambreValidation = [
-    body('numero')
-        .notEmpty()
-        .withMessage('Le numéro de chambre est obligatoire')
-        .isLength({ min: 1, max: 10 })
-        .withMessage('Le numéro doit faire entre 1 et 10 caractères'),
-    body('capacite')
-        .isInt({ min: 1, max: 50 })
-        .withMessage('La capacité doit être un nombre entre 1 et 50')
+ body('numero')
+ .notEmpty()
+ .withMessage('Le numéro de chambre est obligatoire')
+ .isLength({ min: 1, max: 10 })
+ .withMessage('Le numéro doit faire entre 1 et 10 caractères'),
+ body('capacite')
+ .isInt({ min: 1, max: 50 })
+ .withMessage('La capacité doit être un nombre entre 1 et 50')
 ];
 router.get('/', ChambreController.index);
 router.get('/create', ChambreController.create);
--- routes/clients.js
+++ routes/clients.js
@@ -1,46 +1,34 @@
-// routes/clientRoutes.js
-
 import express from 'express';
 import { body } from 'express-validator';
 import ClientController from '../controllers/clientController.js';
 
 const router = express.Router();
 
-// Validation des données du client
+// Validation des données de client
 const clientValidation = [
     body('nom')
         .notEmpty()
-        .withMessage('Le nom du client est obligatoire')
+        .withMessage('Le nom est obligatoire')
         .isLength({ min: 2, max: 50 })
         .withMessage('Le nom doit faire entre 2 et 50 caractères'),
     body('prenom')
         .notEmpty()
-        .withMessage('Le prénom du client est obligatoire')
+        .withMessage('Le prénom est obligatoire')
         .isLength({ min: 2, max: 50 })
         .withMessage('Le prénom doit faire entre 2 et 50 caractères'),
     body('email')
         .notEmpty()
-        .withMessage('L\'email est obligatoire')
+        .withMessage("L'email est obligatoire")
         .isEmail()
-        .withMessage('L\'email doit être valide'),
-    body('telephone')
-        .notEmpty()
-        .withMessage('Le téléphone est obligatoire')
-        .isLength({ min: 10, max: 15 })
-        .withMessage('Le téléphone doit faire entre 10 et 15 caractères'),
-    body('nombre_personnes')
-        .notEmpty()
-        .withMessage('Le nombre de personnes est obligatoire')
-        .isInt({ min: 1, max: 50 })
-        .withMessage('Le nombre de personnes doit être un nombre entre 1 et 50')
+        .withMessage("Le format de l'email n'est pas valide")
 ];
 
-router.get('/clients', ClientController.index);
-router.get('/clients/create', ClientController.create);
-router.post('/clients', clientValidation, ClientController.store);
-router.get('/clients/:id/edit', ClientController.edit);
-router.post('/clients/:id/edit', clientValidation, ClientController.update);
-router.get('/clients/:id/delete', ClientController.delete);
-router.post('/clients/:id/delete', ClientController.destroy);
+router.get('/', ClientController.index);
+router.get('/create', ClientController.create);
+router.post('/', clientValidation, ClientController.store);
+router.get('/:id/edit', ClientController.edit);
+router.put('/:id', clientValidation, ClientController.update);
+router.get('/:id/delete', ClientController.delete);
+router.delete('/:id', ClientController.destroy);
 
-export default router;
+module.exports = router;
